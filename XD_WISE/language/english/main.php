8c935a6b5a341552853ccca6298e05dbaf67a927
grosdunord <grosdunord@gmail.com>
2013-05-29 16:03:27 +0200

    ajout du theme xd wize

---
+++ XD_WISE/language/english/main.php
@@ -0,0 +1,25 @@
+<?php
+
+// Template : HTML5_DEFAULT_BOOTSTRAP
+// Version : 1.0
+// Date : 2013-05-07
+// Auteur : Tatane
+// Support : http://www.xoops-demo.com
+
+define("_THEME_HOME","Home");
+define("_THEME_SEARCH","Search");
+define("_THEME_SEARCH_DESC","Enter your text here");
+define("_THEME_LOGIN","Login");
+define("_THEME_REGISTER","Register");
+define("_THEME_MY_ACCOUNT","My account");
+define("_THEME_ADMIN","Administration");
+define("_THEME_VIEW_ACCOUNT","View account");
+define("_THEME_EDIT_ACCOUNT","Editer mon compte");
+define("_THEME_NOTIFICATION","Notification(s)");
+define("_THEME_YOU_HAVE_MP","You have");
+define("_THEME_YOU_HAVE_NO_MP","You have 0 MP");
+define("_THEME_LOGOUT","Logout");
+define("_THEME_PSEUDO","Pseudo");
+define("_THEME_PASSWORD","Password");
+define("_THEME_FORGOTTEN_PASSWORD","Forgotten password ?");
+?>
---
+++ XD_WISE/language/french/main.php
@@ -0,0 +1,25 @@
+<?php
+
+// Template : HTML5_DEFAULT_BOOTSTRAP
+// Version : 1.0
+// Date : 2013-05-07
+// Auteur : Tatane
+// Support : http://www.xoops-demo.com
+
+define("_THEME_HOME","Accueil");
+define("_THEME_SEARCH","Rechercher");
+define("_THEME_SEARCH_DESC","Entrez votre texte ici");
+define("_THEME_LOGIN","Se connecter");
+define("_THEME_REGISTER","S'inscrire");
+define("_THEME_MY_ACCOUNT","Mon compte");
+define("_THEME_ADMIN","Administration");
+define("_THEME_VIEW_ACCOUNT","Voir mon compte");
+define("_THEME_EDIT_ACCOUNT","Editer mon compte");
+define("_THEME_NOTIFICATION","Notification(s)");
+define("_THEME_YOU_HAVE_MP","Vous avez");
+define("_THEME_YOU_HAVE_NO_MP","Vous avez 0 MP");
+define("_THEME_LOGOUT","Déconnexion");
+define("_THEME_PSEUDO","Pseudo");
+define("_THEME_PASSWORD","Mot de passe");
+define("_THEME_FORGOTTEN_PASSWORD","Mot de passe oublié ?");
+?>
